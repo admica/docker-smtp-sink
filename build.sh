541d92accac94dd3465d96fb869ab516bf0f1f57
admica <root@localhost>
2019-09-30 15:30:56 -0500

    added image build and container run scripts

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# build the image
+docker image build -t smtp_sink:1.0 .
+
+# show it in docker
+docker images | grep smtp_sink
+
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run the container
+docker-compose smtp_sink up -d
+
